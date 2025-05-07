17d4b72ed4b537dbd254698bcdd85cdf0fea1831
Wolf1610 <dutta2772004@gmail.com>
2025-05-07 19:14:27 +0530

    init

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import bcrypt from "bcrypt";
+
+import { prisma } from "@repo/db/client";
+
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hi there")
+});
+
+app.post("/signup", async (req, res)=> {
+    const { username, password } = req.body; 
+    const hashPassword = await   bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+        data: {
+            username,
+            password: hashPassword,
+        }
+    });
+
+    res.json({
+        message: "Sign Up successful"
+    })
+});
+
+
+
+app.listen(4000);
